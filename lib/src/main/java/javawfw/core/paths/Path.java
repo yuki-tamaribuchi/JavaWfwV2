08c59e9535bce94e81f7b1662ff9fe253dd0e2fe
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-06-29 07:52:11 +0900

    Modify Path contructor

--- lib/src/main/java/javawfw/core/paths/Path.java
+++ lib/src/main/java/javawfw/core/paths/Path.java
@@ -3,13 +3,18 @@ package javawfw.core.paths;
 import java.util.regex.Pattern;
 import java.util.regex.Matcher;
 
+import javawfw.http.HttpMethod;
+
+
 public class Path {
-	public Path(String path) {
 	private String path;
+	private HttpMethod httpMethod;
 	private Pattern regexPattern;
 	
-	public Path(String path) {
+	public Path(String path, HttpMethod httpMethod) {
 		this.path = path;
+		this.httpMethod = httpMethod;
+		regexPattern = getRegexPattern(path);
 	}
 
 	private Pattern getRegexPattern(String path) {
