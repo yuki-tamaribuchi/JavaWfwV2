47f5e14b015c197bfc95e25c2c4fc5da27f89651
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-07-03 19:36:05 +0900

    Add component scan call

--- lib/src/main/java/javawfw/JavaWfw.java
+++ lib/src/main/java/javawfw/JavaWfw.java
@@ -1,5 +1,13 @@
 package javawfw;
 
+import java.util.Map;
+
+import javawfw.core.components.ComponentScanner;
 
 public class JavaWfw {
+	private JavaWfw(String ipAddr, int port, Class<?> clazz) {
+
+		Map<Class<?>,Object> components = ComponentScanner.scan(clazz.getPackageName());
+		System.out.println("Registered Components => " + components);
+	}
 }
