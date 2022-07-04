c9d9df3d47c2422d28329777b405a31f478e036a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-07-05 08:05:18 +0900

    Add PathRegister call

--- lib/src/main/java/javawfw/JavaWfw.java
+++ lib/src/main/java/javawfw/JavaWfw.java
@@ -1,13 +1,20 @@
 package javawfw;
 
+import java.util.List;
 import java.util.Map;
 
 import javawfw.core.components.ComponentScanner;
+import javawfw.core.paths.PathRegister;
+import javawfw.core.paths.Path;
 
 public class JavaWfw {
+	private Server server;
+
 	private JavaWfw(String ipAddr, int port, Class<?> clazz) {
 
 		Map<Class<?>,Object> components = ComponentScanner.scan(clazz.getPackageName());
 		System.out.println("Registered Components => " + components);
+		List<Path> paths = PathRegister.register(components);
 	}
+		
 }
