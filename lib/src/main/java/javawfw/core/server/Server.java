23c209123cab44f68f6eac87ee8151469d71f066
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-07-06 08:50:54 +0900

    Add components and pathResolver to Server constructor

--- lib/src/main/java/javawfw/core/server/Server.java
+++ lib/src/main/java/javawfw/core/server/Server.java
@@ -2,17 +2,25 @@ package javawfw.core.server;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Map;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.Headers;
 
+import javawfw.core.paths.PathResolver;
+
 
 public class Server {
 	HttpServer server;
+	private static Map<Class<?>, Object> components;
+	private static PathResolver pathResolver;
 
-	public Server(String ipAddr, int port) {
+	public Server(String ipAddr, int port, Map<Class<?>, Object> components, PathResolver pathResolver) {
+		Server.pathResolver = pathResolver;
+		Server.components = components;
+
 		try {
 			server = HttpServer.create(new InetSocketAddress(ipAddr, port), 0);
 			server.createContext("/", new Handler());
