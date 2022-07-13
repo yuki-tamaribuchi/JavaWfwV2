4883c25523d04f1275bc3aab36f18d80eb1c017a
TAMARIBUCHI Yuki <uk.tmbc2408@gmail.com>
2022-07-14 07:59:47 +0900

    Add createInvokeParameterList function

--- lib/src/main/java/javawfw/core/server/Server.java
+++ lib/src/main/java/javawfw/core/server/Server.java
@@ -3,7 +3,13 @@ package javawfw.core.server;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.util.Map;
+import java.util.stream.Stream;
 import java.io.OutputStream;
+import java.lang.reflect.Parameter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.lang.reflect.Method;
 
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
@@ -13,8 +19,11 @@ import com.sun.net.httpserver.Headers;
 import javawfw.core.paths.PathResolver;
 import javawfw.http.Status;
 import javawfw.http.response.IResponse;
+import javawfw.annotations.PathVariable;
+import javawfw.annotations.RequestParam;
 import javawfw.core.paths.Path;
 import javawfw.http.response.StatusResponse;
+import javawfw.core.exceptions.paths.NullPathVariableMapException;
 
 
 public class Server {
@@ -55,6 +64,38 @@ public class Server {
 				normalizedUrl = normalizedUrl.substring(0, normalizedUrl.length()-1);
 			}
 		return normalizedUrl;
+	}
+
+	private static List<Object> createInvokeParameterList(Method method, Path path, String url, String requestBody) throws NullPathVariableMapException {
+		List<Object> invokeParameterList = new ArrayList<>();
+
+		List<Parameter> parameters = Arrays.asList(method.getParameters());
+		Stream<Parameter> parametersSteam = parameters.stream();
+		parametersSteam
+		.forEach(param->{
+			if (param.isAnnotationPresent(PathVariable.class)) {
+				Map<String, String> pathVariableMap = path.getPathVariableMap(url.split("\\?")[0]);
+				if (pathVariableMap == null) {
+					throw new NullPathVariableMapException();
+				}
+
+				PathVariable pathVariable = param.getAnnotation(PathVariable.class);
+				String pathVariableName = pathVariable.value();
+				String pathVariableValue = pathVariableMap.get(pathVariableName);
+				invokeParameterList.add(pathVariableValue);
+			} else if (param.isAnnotationPresent(RequestParam.class)) {
+				Map<String, String> requestParamMap = path.getRequestParamMap(url);
+
+				RequestParam requestParam = param.getAnnotation(RequestParam.class);
+				String requestParamName = requestParam.value();
+				String requestParamValue = requestParamMap.get(requestParamName);
+				invokeParameterList.add(requestParamValue);
+			} else if (param.isAnnotationPresent(RequestBody.class)) {
+				invokeParameterList.add(requestBody);
+			}
+		});
+		return invokeParameterList;
+	}
 
 	private static void sendResponse(IResponse response, HttpExchange t) {
 		int status = response.getStatus();
